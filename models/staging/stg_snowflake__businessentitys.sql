d5323f13dbeb41ef1a5211150c3923a25a908a0b
WILLIAN DOS SANTOS <willianrds93@Hotmail.com>
2024-07-08 20:17:06 +0000

    Criando algumas dimensoes de store, e clientes

---
+++ models/staging/stg_snowflake__businessentitys.sql
@@ -0,0 +1,9 @@
+with
+    source_businessentitys as (
+        select cast(businessentityid as int) as pk_businessentityid
+        from {{ source("snowflake", "businessentity") }}
+
+    )
+
+select *
+from source_businessentitys
