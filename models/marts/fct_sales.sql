5ad7bab81b3390a9278eb48bcdacd7ae1ed4d1e2
WILLIAN DOS SANTOS <willianrds93@Hotmail.com>
2024-07-10 21:41:02 +0000

    Ajustando fatos vendas

--- models/marts/fct_sales.sql
+++ models/marts/fct_sales.sql
@@ -1,23 +1,24 @@
 WITH fact_sales_keys_only AS (
-    SELECT
-        oi.FK_CUSTOMERID,
-        oi.fk_addressid,
-        oi.FK_SALESPERSONID,
-        oi.FK_TERRITORYID,
-        oi.FK_CREDITCARDID,
-        oi.OH_ORDERDATE,
-        oi.OH_STATUS,
-        oi.PK_SALESORDERDETAILID,
-        oi.FK_PRODUCTID,
-        oi.FK_SPECIALOFFERID,
-        oi.OD_ORDERQTY,
-        oi.OD_UNITPRICE,
-        oi.OD_UNITPRICEDISCOUNT,
-        oi.FREIGHT_PER_ITEM,
-        oi.TAX_PER_ITEM,
-        oi.ITEM_TOTAL,
-        oi.DISCOUNTED_ITEM_TOTAL,
-        oi.OH_TOTALDUE
+    SELECT    
+        PK_SALESORDERDETAILID,
+        FK_ADDRESSID,
+        PK_SALESORDERID,
+        FK_CUSTOMERID,
+        FK_SALESPERSONID,
+        FK_TERRITORYID,
+        FK_CREDITCARDID,
+        OH_ORDERDATE,
+        OH_STATUS,
+        FK_PRODUCTID,
+        FK_SPECIALOFFERID,
+        OD_ORDERQTY,
+        OD_UNITPRICE,
+        OD_UNITPRICEDISCOUNT,
+        FREIGHT_PER_ITEM,
+        TAX_PER_ITEM,
+        ITEM_TOTAL,
+        DISCOUNTED_ITEM_TOTAL,
+        OH_TOTALDUE
     FROM {{ ref('dim_ordered_items') }} oi
 )
 SELECT * FROM fact_sales_keys_only
